363f7b6f21326222107686e71cc893f06f350fdf
zhgkongming <zhgkongming@gmail.com>
2025-08-19 15:57:27 +0000

    up

---
+++ init_git.sh
@@ -0,0 +1,7 @@
+echo "# firebase" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/beefree365/firebase.git
+git push -u origin main
